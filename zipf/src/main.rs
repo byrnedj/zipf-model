a08d06be7ecadf7503e0a759e21e8b777ec8eadb
Wesley Smith <wjs@Wesleys-MacBook-Pro.local>
2020-03-10 17:22:12 -0400

    rust script implementation

--- zipf/src/main.rs
+++ zipf/src/main.rs
@@ -1,32 +1,66 @@
 use std::env;
-//TODO: GMP crate
+use rug::{Integer, Float}
 
-fn compute_normalizer(alpha: f32, m: u32) -> f32 {
-    //TODO
+
+fn compute_normalizer(alpha: Float, m: u32) -> Float {
+    let mut total = Float::new();
+    let mut temp = Float::new();
+    let mut induction = Float::new();
+
+    for i in 1..(m+1) {
+        induction.assign(i);
+        temp.assign(induction.pow(-1*alpha))
+        total.assign(total + temp)
+    }
+    
+    1/total
 }
 
-fn populate_derivatives(m: u32, n: u32) {
-    //TODO
+
+fn populate_subexpressions(m: u32, alpha: Float, norm: Float) -> ([Float; m], [Float; m]) {
+    let mut expr: [Float, m];
+    let mut ln_expr: [Float, m];
+    let mut induction = Float::new();
+
+    for i in 1..(m+1) {
+        expr[i-1].assign(1 - norm*(i.pow(-1*alpha)));
+        ln_expr[i-1].assign(-1*expr[i-1].ln())
+    }
+
+    (expr, ln_expr)
 }
 
-fn populate_subexpressions(m: u32, alpha: f32, norm: f32) {
-    //TODO
-}
+fn populate_footprints_derivatives(m: u32, n: u32, expr: [Float; m], ln_expr: [Float; m]) -> ([Float; n], [Float; n]){
+    
+    let mut fp: [Float, n];
+    let mut drv: [Float, n];
 
-fn populate_footprint(m: u32, n: u32) {
-    //TODO
+    for i in 0..n {
+        fp[i].assign(0);
+        drv[i].assign(0);
+        for j in 0..m {
+            fp[i].assign(fp[i] + (1 - expr[j].pow(i)))
+            drv[i].assign(drv[i] + (ln_expr[j]*(expr[j].pow(i))))
+        }
+    }
+
+    (fp, drv)
 }
 
 fn compute_mrs(m: u32, n: u32, alpha: f32) {
     let norm: f32 = compute_normalizer(alpha, m);
-    populate_subexpressions(m, alpha, norm);
-    populate_footprint(m, n);
-    populate_derivatives(m, n);
+    let (expr, ln_expr) : ([Float; m], [Float; m]) = populate_subexpressions(m, alpha, norm);
+    let (fp, drv) : ([Float; n], [Float; n]) = populate_footprints_derivatives(m, n, expr, ln_expr);
     println!("parameters: data size = {0}, trace length = {1}, zipf parameter = {2}", m, n, alpha);
 
-    let cache_size: u32 = 0;
+    let mut cache_size: u32 = 0;
 
-    //TODO
+    for x in 0..(n-1):
+        if fp[x].floor() > cache_size {
+            println!("cache size: {0} \n    miss ratio: {1}", fp[x], drv[x]);
+            cache_size = math.floor(fp[x]);
+        }
+
 }
 
 
@@ -34,8 +68,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let m: u32 = &args[1].parse().unwrap();
     let n: u32 = &args[2].parse().unwrap();
-    let alpha: f32 = &args[3].parse().unwrap();
-    compute_mrs(m, n, alpha);
+    let alpha = Float::new();
+    alpha.assign(&args[3].parse().unwrap());
 
+    compute_mrs(m, n, alpha);
 
 }
