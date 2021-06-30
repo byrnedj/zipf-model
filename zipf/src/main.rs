84c0d40367680a88884e36adcf35125961e1a2fa
wes-smith <wes.apps.2014@gmail.com>
2021-06-30 15:31:45 -0400

    removed Float library

--- zipf/src/main.rs
+++ zipf/src/main.rs
@@ -6,15 +6,15 @@ use clap::{Arg, App};
 use std::fs::File;
 use std::io::Write;
 
-fn compute_normalizer(alpha: &Float, m: &u32) -> Float {
-    let mut total = Float::new(100);
-    let mut temp = Float::new(100);
-    let mut induction = Float::new(100);
+fn compute_normalizer(alpha: &f64, m: &u32) -> f64 {
+    let mut total: f64 = 0.0;
+    let mut temp: f64 = 0.0;
+    let mut induction: f64 = 0.0;
 
     for i in 1..(m+1) {
-        induction.assign(i);
-        temp.assign((&induction).pow(-1*(alpha.clone())));
-        total.assign(total.clone() + temp.clone());
+        induction = i as f64;
+        temp = (&induction).pow(-1.0*(alpha.clone()));
+        total = total + temp;
     }
     let norm = 1.0/total;
     
@@ -22,20 +22,20 @@ fn compute_normalizer(alpha: &Float, m: &u32) -> Float {
 }
 
 
-fn populate_subexpressions(m: &u32, alpha: &Float, norm: &Float) -> (Vec<Float>, Vec<Float>, Vec<Float>) {
-    let mut expr: Vec<Float> = Vec::new();
-    let mut ln_expr: Vec<Float> = Vec::new();
-    let mut binom_expr: Vec<Float> = Vec::new();
+fn populate_subexpressions(m: &u32, alpha: &f64, norm: &f64) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
+    let mut expr: Vec<f64> = Vec::new();
+    let mut ln_expr: Vec<f64> = Vec::new();
+    let mut binom_expr: Vec<f64> = Vec::new();
 
     for i in 1..(m+1) {
-        let mut temp = Float::new(100);
-        temp.assign(i);
-        expr.push(Float::new(100));
-        ln_expr.push(Float::new(100));
-        binom_expr.push(Float::new(100));
-        expr[(i-1) as usize].assign(1 - norm*(temp.clone().pow(-1*(alpha.clone()))));
-        ln_expr[(i-1) as usize].assign(-1*expr[(i-1) as usize].clone().ln());
-        binom_expr[(i-1) as usize].assign(norm*(temp.clone().pow(-1*(alpha.clone()))));
+        let mut temp: f64 = 0.0;
+        temp = i as f64;
+        expr.push(0.0);
+        ln_expr.push(0.0);
+        binom_expr.push(0.0);
+        expr[(i-1) as usize] = 1.0 - norm*(temp.clone().pow(-1.0*(alpha.clone())));
+        ln_expr[(i-1) as usize] = -1.0*expr[(i-1) as usize].clone().ln();
+        binom_expr[(i-1) as usize] = norm*(temp.clone().pow(-1.0*(alpha.clone())));
     }
 
     (expr, ln_expr, binom_expr)
@@ -83,13 +83,13 @@ fn populate_footprints_derivatives(m: &u32, n: &u32, alpha: &Float, norm:&Float,
     (fp, drv, rdfp)
 }
 
-fn populate_approx(m: &u32, n: &u32, alpha: &Float, norm: &Float, r: &f32, c: u32) -> (Float, Float, Float){
-    let mut fp_approx = Float::new(100);
-    let mut drv_approx = Float::new(100);
-    let mut wfp_approx = Float::new(100);
-    let mut rdfp_approx = Float::new(100);
+fn populate_approx(m: &u32, n: &u32, alpha: &f64, norm: &f64, r: &f32, c: u32) -> (f64, f64, f64){
+    let mut fp_approx: f64 = 0.0;
+    let mut drv_approx: f64 = 0.0;
+    let mut wfp_approx: f64 = 0.0;
+    let mut rdfp_approx: f64 = 0.0;
 
-    let inva: f64 = 1.0/(alpha.to_f64());
+    let inva: f64 = 1.0/alpha;
     let mut searchup = false;
     let mut searchdown = false;
     let mut win_length: f64 = 1.0;
@@ -97,7 +97,7 @@ fn populate_approx(m: &u32, n: &u32, alpha: &Float, norm: &Float, r: &f32, c: u3
     let mut t_final: f64 = 0.0;
 
     while !searchup{
-        let t: f64 = norm.to_f64() * (win_length as f64)/(*m as f64).pow(alpha.to_f64());
+        let t: f64 = norm * (win_length as f64)/(*m as f64).pow(alpha);
         let fp_temp: f64 = (*m as f64)*(1.0 - (inva*t.pow(inva)*rgsl::gamma_beta::incomplete_gamma::gamma_inc(-1.0*inva, t)));
         if fp_temp > (c as f64){
             lower_bound = win_length/2.0;
@@ -109,8 +109,8 @@ fn populate_approx(m: &u32, n: &u32, alpha: &Float, norm: &Float, r: &f32, c: u3
     }
 
     while !searchdown{
-        let t: f64 = norm.to_f64() * ((win_length + lower_bound)/2.0)/(*m as f64).pow(alpha.to_f64());
-        let t2: f64 = norm.to_f64() * (((win_length + lower_bound)/2.0) - 1.0)/(*m as f64).pow(alpha.to_f64());
+        let t: f64 = norm * ((win_length + lower_bound)/2.0)/(*m as f64).pow(alpha);
+        let t2: f64 = norm * (((win_length + lower_bound)/2.0) - 1.0)/(*m as f64).pow(alpha);
         let fp_temp: f64 = (*m as f64)*(1.0 - (inva*t.pow(inva)*rgsl::gamma_beta::incomplete_gamma::gamma_inc(-1.0*inva, t)));
         let fp_temp2: f64 = (*m as f64)*(1.0 - (inva*t.pow(inva)*rgsl::gamma_beta::incomplete_gamma::gamma_inc(-1.0*inva, t2)));
 
@@ -128,37 +128,37 @@ fn populate_approx(m: &u32, n: &u32, alpha: &Float, norm: &Float, r: &f32, c: u3
     }
 
     //TODO: fp approx, drv approx
-    fp_approx.assign((*m as f64)*(1.0 - (inva*t_final.pow(inva)*rgsl::gamma_beta::incomplete_gamma::gamma_inc(-1.0*inva, t_final))));
-    drv_approx.assign(((win_length as f64*norm.to_f64()).pow(inva)*rgsl::gamma_beta::incomplete_gamma::gamma_inc(1.0 - inva, t_final))/(alpha.to_f64()*win_length as f64));
+    fp_approx = (*m as f64)*(1.0 - (inva*t_final.pow(inva)*rgsl::gamma_beta::incomplete_gamma::gamma_inc(-1.0*inva, t_final)));
+    drv_approx = ((win_length as f64*norm).pow(inva)*rgsl::gamma_beta::incomplete_gamma::gamma_inc(1.0 - inva, t_final))/(alpha*win_length as f64);
 
 
     //wfp approximation using definite integral
-    let mut norm_temp = Float::new(100);
-    let mut temp1 = Float::new(100);
-    let mut tempM = Float::new(100);
-    let mut exp1 = Float::new(100);
-    let mut expM = Float::new(100);
+    let mut norm_temp: f64 = 0.0;
+    let mut temp1: f64 = 0.0;
+    let mut tempM: f64 = 0.0;
+    let mut exp1: f64 = 0.0;
+    let mut expM: f64 = 0.0;
     let mReciprocal: f64 = 1.0/(*m as f64);
-    let mut expMtemp = Float::new(100);
-    let n_func: f64 = 1.0 + (1.0/alpha.to_f64());
+    let mut expMtemp: f64 = 0.0;
+    let n_func: f64 = 1.0 + (1.0/alpha);
 
-    expMtemp.assign(mReciprocal.pow(alpha));
-    norm_temp.assign(norm*(-1.0*(win_length as f64)));
-    temp1.assign(norm_temp.clone()*((1.0-r).ln()));
-    tempM.assign(norm_temp.clone()*expMtemp*((1.0-r).ln()));
+    expMtemp = mReciprocal.pow(alpha);
+    norm_temp = norm*(-1.0*(win_length as f64));
+    temp1 = norm_temp.clone()*((1.0-(*r as f64)).ln());
+    tempM = norm_temp.clone()*expMtemp*((1.0-(*r as f64)).ln());
 
-    exp1.assign((temp1.clone().to_f64().pow(n_func - 1.0))*rgsl::gamma_beta::incomplete_gamma::gamma_inc(1.0 - n_func, temp1.to_f64()));
-    expM.assign(((*m as f64)*tempM.clone().pow(n_func - 1.0)) * rgsl::gamma_beta::incomplete_gamma::gamma_inc(1.0 - n_func, tempM.to_f64()));
+    exp1 = (temp1.clone().pow(n_func - 1.0))*rgsl::gamma_beta::incomplete_gamma::gamma_inc(1.0 - n_func, temp1);
+    expM =((*m as f64)*tempM.clone().pow(n_func - 1.0)) * rgsl::gamma_beta::incomplete_gamma::gamma_inc(1.0 - n_func, tempM);
 
-    wfp_approx.assign(m - 1 - (expM.clone()- exp1.clone())/alpha);
+    wfp_approx = (*m as f64) - 1.0 - (expM.clone()- exp1.clone())/alpha;
 
-    rdfp_approx.assign(wfp_approx/fp_approx.clone());
+    rdfp_approx = wfp_approx/fp_approx.clone();
     
     (fp_approx, drv_approx, rdfp_approx)
 }
 
-fn compute_approx(m: u32, n: u32, alpha: Float, r: f32, output_int: u32, use_stdout: u32, ofilename: &str, c: u32, norm: Float) {
-    let (fp_approx, drv_approx, rdfp_approx) : (Float, Float, Float) = populate_approx(&m, &n, &alpha, &norm, &r, c);
+fn compute_approx(m: u32, n: u32, alpha: f64, r: f32, output_int: u32, use_stdout: u32, ofilename: &str, c: u32, norm: f64) {
+    let (fp_approx, drv_approx, rdfp_approx) : (f64, f64, f64) = populate_approx(&m, &n, &alpha, &norm, &r, c);
     let mut of;
     if use_stdout == 0 {
         of = File::create(ofilename).expect("ERROR!");
@@ -176,10 +176,10 @@ fn compute_approx(m: u32, n: u32, alpha: Float, r: f32, output_int: u32, use_std
     println!("size,mr,wbr");
     
     let fsize = output_int as f64;
-    let size = round::floor(fp_approx.to_f64(), fsize.log10() as u8) as u32;
+    let size = round::floor(fp_approx, fsize.log10() as u8) as u32;
     let isize = (size/output_int)*output_int;
-    let mr = round::floor(drv_approx.to_f64(), 3);
-    let wbr = round::floor(rdfp_approx.to_f64(), 3) * mr;
+    let mr = round::floor(drv_approx, 3);
+    let wbr = round::floor(rdfp_approx, 3) * mr;
     //if use_stdout == 0 {
     //    let line = format!("{0},{1:.3},{2:.3}\n",isize,mr,wbr);
     //    of.write(line.as_bytes()).expect("WRITE_ERROR");
@@ -192,23 +192,23 @@ fn compute_approx(m: u32, n: u32, alpha: Float, r: f32, output_int: u32, use_std
     
 }
 
-fn compute(m: u32, n: u32, alpha: Float, r: f32) {
-    let norm: Float = compute_normalizer(&alpha, &m);
-    let (expr, ln_expr, binom_expr) : (Vec<Float>, Vec<Float>, Vec<Float>) = populate_subexpressions(&m, &alpha, &norm);
-    let (fp, drv, rdfp) : (Vec<Float>, Vec<Float>, Vec<Float>) = populate_footprints_derivatives(&m, &n, &alpha, &norm, &r, expr, ln_expr, binom_expr);
+fn compute(m: u32, n: u32, alpha: f64, r: f32) {
+//    let norm: Float = compute_normalizer(&alpha, &m);
+//    let (expr, ln_expr, binom_expr) : (Vec<Float>, Vec<Float>, Vec<Float>) = populate_subexpressions(&m, &alpha, &norm);
+//    let (fp, drv, rdfp) : (Vec<Float>, Vec<Float>, Vec<Float>) = populate_footprints_derivatives(&m, &n, &alpha, &norm, &r, expr, ln_expr, binom_expr);
 
-    let mut cache_size: u32 = 0;
-    for x in 0..(n-1) {
-        if fp[x as usize].clone().floor() > cache_size {
-            let size = round::floor(fp[x as usize].to_f64(),1);
-            if size.fract() == 0.0 {
-                let mr = round::floor(drv[x as usize].to_f64(),3);
-                let wbr = round::floor(rdfp[x as usize].to_f64(),3) * mr;
-                println!("{0},{1:.3},{2:.3}",size,mr,wbr);
-            }
-            cache_size = fp[x as usize].clone().floor().to_u32_saturating().unwrap();
-        }
-    }
+//    let mut cache_size: u32 = 0;
+//    for x in 0..(n-1) {
+//        if fp[x as usize].clone().floor() > cache_size {
+//            let size = round::floor(fp[x as usize].to_f64(),1);
+//            if size.fract() == 0.0 {
+//                let mr = round::floor(drv[x as usize].to_f64(),3);
+//                let wbr = round::floor(rdfp[x as usize].to_f64(),3) * mr;
+//                println!("{0},{1:.3},{2:.3}",size,mr,wbr);
+//            }
+//            cache_size = fp[x as usize].clone().floor().to_u32_saturating().unwrap();
+//        }
+//    }
 }
 
 
@@ -265,10 +265,10 @@ fn main() {
 
     //parse clap clargs
     let ex_type = matches.value_of("type").unwrap();
-    let mut alpha = Float::new(100);
+    let mut alpha: f64 = 0.0;
     let a_str: &str = matches.value_of("alpha").unwrap();
-    let a = a_str.parse::<f32>().unwrap();
-    alpha.assign(a);
+    let a = a_str.parse::<f64>().unwrap();
+    alpha = a;
     let m_str: &str = matches.value_of("datasize").unwrap();
     let m: u32 = m_str.parse::<u32>().unwrap();
     let n_str: &str = matches.value_of("tracelength").unwrap();
@@ -302,7 +302,7 @@ fn main() {
     }
 
     else if ex_type.eq("approx"){
-        let norm: Float = compute_normalizer(&alpha, &m);
+        let norm: f64 = compute_normalizer(&alpha, &m);
         println!("size,mr,wbr");
         for c in 0..cache_sizes.len(){
             compute_approx(m, n, alpha.clone(), r, output_int, use_stdout, ofilename, cache_sizes[c], norm.clone());
